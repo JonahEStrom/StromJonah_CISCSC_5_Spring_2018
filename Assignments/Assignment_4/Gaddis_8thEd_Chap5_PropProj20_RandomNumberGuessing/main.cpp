6ed87b71b27ab90fbf94c76ae2c6c228dc25b260
JonahEStrom <Jonahestrom@gmail.com>
2018-04-03 09:34:16 -0700

    Signed-off-by: JonahEStrom <Jonahestrom@gmail.com>

---
+++ Assignments/Assignment_4/Gaddis_8thEd_Chap5_PropProj20_RandomNumberGuessing/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on July 19, 2016, 9:07 AM
+ * Purpose:  Hello World Template
+ */
+
+//System Libraries Here
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    float rng;
+    
+    //Input or initialize values Here
+    unsigned seed = time(0);
+    srand(seed);
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout<<rand()<<endl;
+
+    //Exit
+    return 0;
+}
+
